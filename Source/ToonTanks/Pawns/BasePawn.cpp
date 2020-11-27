37f6804a79c18e7c8d4219f729536c6f521d89c8
Cristian Campos <camposcristian.cjc@gmail.com>
2020-11-26 22:30:20 -0500

    Added Components to BasePawn
    Created Capsule component, Base and Turret mesh components, and ProjectileSpawnPoint, each necessary for tank control.

--- Source/ToonTanks/Pawns/BasePawn.cpp
+++ Source/ToonTanks/Pawns/BasePawn.cpp
@@ -2,12 +2,29 @@
 
 
 #include "BasePawn.h"
+#include "Components/CapsuleComponent.h"
 
 // Sets default values
 ABasePawn::ABasePawn()
 {
  	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
+
+	// This whole section sets up behavioral hierarchy for how components will act based on what they inherit
+
+	CapsuleComp = CreateDefaultSubobject<UCapsuleComponent>(TEXT("Capsule Collider"));
+	RootComponent = CapsuleComp;
+
+	BaseMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Base Mesh"));
+	// b/c we want to inherit the movement and rotation of capsule component (that's what we'll be moving around the world)
+	// we set the basemesh as an attachement of RootComponent so it gets same behavior.
+	BaseMesh->SetupAttachment(RootComponent);
+
+	TurretMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Turret Mesh"));
+	TurretMesh->SetupAttachment(BaseMesh);
+
+	ProjectileSpawnPoint = CreateDefaultSubobject<USceneComponent>(TEXT("Projectile Spawn Point"));
+	ProjectileSpawnPoint->SetupAttachment(TurretMesh);
 
 }
 
--- Source/ToonTanks/Pawns/BasePawn.h
+++ Source/ToonTanks/Pawns/BasePawn.h
@@ -6,24 +6,42 @@
 #include "GameFramework/Pawn.h"
 #include "BasePawn.generated.h"
 
+class UCapsuleComponent;
+
 UCLASS()
 class TOONTANKS_API ABasePawn : public APawn
 {
 	GENERATED_BODY()
 
+private:
+
+// capsule component needs to be forward declared so as to minimize #includes and increase runtime
+UPROPERTY()
+UCapsuleComponent* CapsuleComp;
+
+// UStaticMeshComponent is used to create an instance of a UStaticMesh which is an object composed
+// of static polygons
+UPROPERTY()
+UStaticMeshComponent* BaseMesh;
+UPROPERTY()
+UStaticMeshComponent* TurretMesh;
+
+// useful for defining where the projectile will spawn :)
+UPROPERTY()
+USceneComponent* ProjectileSpawnPoint;
+
 public:
 	// Sets default values for this pawn's properties
 	ABasePawn();
 
-protected:
-	// Called when the game starts or when spawned
-	virtual void BeginPlay() override;
-
-public:	
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
 
 };
